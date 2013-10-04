5921b3fa5d42159e81fe17b9f3ad81ebdac727d4
Robert Emerson <eggbert.remerson@gmail.com>
2013-10-04 15:25:03 -0400

    Second commit of client code, detailing some methods

--- client.py
+++ client.py
@@ -2,8 +2,19 @@ __author__ = 'robert'
 
 #Contains the code for Team 18's dropbox client
 
+
 def main():
-    print 'Hello world'
+    #check for config file at user's home directory (start it with . to be hidden)
+    #if not found, prompt user to register
+    pass
+
+def db_connect():
+    #code for database connection goes here
+    pass
+
+def add_user():
+    #code for adding a user to the database goes here
+    pass
 
 if __name__ == '__main__':
     main()
