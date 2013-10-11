922b867c33b3c74fff1912a0356d0ed1f8c3ae85
MisterAbc <zl4ry@virginia.edu>
2013-10-10 21:15:31 -0400

    Added config file reading in the config file

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+import os
+import flask
 __author__ = 'robert'
 
 #Contains the code for Team 18's dropbox client
@@ -22,16 +24,28 @@ def create_new_user():
 
 
 def create_user_directory():
+    # prompt user for home directory location
+    # return the location
     pass
 
+def initiate_config_file(config_file, initial_dir):
+    pass
 
 def main():
+    filename = os.path.expanduser("~/.onedirc")
+    config = None
+    new_user = False #A user is new if they haven't registered
+    try:
+        config = open(filename, 'r')
+    except IOError:
+        config = open(filename, 'w')
+        new_user = True
     #check for config file at user's home directory (start it with . to be hidden)
     #if doesn't exist prompt user to login or register
-    new_user = False #A user is new if they haven't registered
     if new_user:
         create_new_user()
-        create_user_directory()
+        dirname = create_user_directory()
+        initiate_config_file(config, dirname)
     else:
         #read config file
         #start syncing files, client should persist (not sprint 1 work)
