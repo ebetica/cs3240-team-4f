cbac809de3c5c228a668e66a1531c6d3818e2de7
robert <roe2pj@virginia.edu>
2013-10-16 13:01:46 -0400

    Fixed errors (Thanks PyCharm for not highlighting them). Also moved code around for organizational purposes.

--- prototypes/file_updates.py
+++ prototypes/file_updates.py
@@ -1,20 +1,26 @@
 __author__ = 'robert'
 
-import hashlib,os, pickle, sys, threading, time
+import hashlib
+import os
+import pickle
+import sys
+import threading
+import time
+
 
 #Prototype to compare files on disk to those stored on a server
 #Download listing of files on server -> create listing of files on local machine -> compare listings -> update necessary files
 class FileChecker:
-    path = '' #path to Onedir directory
-    interval = 5 #minutes between checking for updates
+    path = ''  # path to Onedir directory
+    interval = 5  # minutes between checking for updates
 
-    def __init__(self):
+    def __init__(self, pathname=None, intervalIn=None):
         userhome = os.environ['HOME']
-        path = userhome + '/Onedir'
+        self.path = userhome + '/Onedir'
 
     def __init__(self, pathname, intervalIn):
-        path = pathname
-        interval = intervalIn
+        self.path = pathname
+        self.interval = intervalIn
 
     #Makes sure the onedir directory exists on the local machine
     #If it doesn't, this creates the directory and returns false
@@ -22,41 +28,41 @@ class FileChecker:
         if not os.path.isdir(self.path):
             try:
                 os.mkdir(self.path, 0700)
-            except:
+            except OSError:
                 sys.exit('Cannot create onedir directory')
             return False
         return True
 
     #Safe way to hash large files (reads and hashes in chunks)
     #From www.pythoncentral.io/hashing-files-with-python/
-    def safeHashFile(file):
+    def safeHashFile(self, fileOb):
         BLOCKSIZE = 65536
         hasher = hashlib.md5()
-        with open(file, 'rb') as afile:
+        with open(fileOb, 'rb') as afile:
             buf = afile.read(BLOCKSIZE)
             while len(buf) > 0:
-                hash.update(buf)
+                hasher.update(buf)
                 buf = afile.read(BLOCKSIZE)
         return hasher.hexdigest()
 
     #Builds a dictionary of the local files on the machine
     #Uses filename as key, and stores an md5 hash of each file as well as when the file was last modified
-    def get_local_files(self):
+    def get_local_files(self, fileOb):
         try:
-            filelist = os.listdir(self.path)
+            filelist = os.listdir(fileOb)
         except OSError:
             filelist = {}
-        dict = {}
-        for file in filelist:
-            if os.path.isfile(file):
-                dict += {file, [self.safeHashFile(file), os.path.getmtime(file)]}
+        fileDict = {}
+        for fileOb in filelist:
+            if os.path.isfile(fileOb):
+                fileDict += {fileOb, [self.safeHashFile(file), os.path.getmtime(file)]}
             else:
-                dict += self.get_local_files(self.path)
-        return dict
+                fileDict += self.get_local_files(fileOb)
+        return fileDict
 
-    #Loads a dictionary of files stored on the server using pickle
+    #Loads a dictionary of (files stored on the server) from Onedir directory using pickle
     def load_server_files(self):
-        path = self.path + '/.onedirdata.p'
+        path = self.path + '/.onedirdata.p'  # Local machine path to dictionary of files from server
         if os.path.isfile(path):
             return pickle.load(open(path))
         else:
@@ -64,53 +70,53 @@ class FileChecker:
 
     #Connects to server, downloads a listing of the user's files and then returns that listing
     def get_server_files(self):
-        path = ''
         #get_update_files(server, username)
-            #build path on server from username
-            #downloads manifest of user files on server to $ONEDIR/.onedirdata.p
-        return self.load_server_files(path)
+        #build path on server from username
+        #downloads manifest of user files on server to $ONEDIR/.onedirdata.p
+        return self.load_server_files()
 
     #Compares the file listings between server and local machine
     #Returns a list containing two lists
     #The first list is a list of files that need to be updated on the local machine
     #The second list is a list of files that need to be updated on the server
     def compareManifests(self, local, server):
-        dict= {}
         serverUpdates = []
         localUpdates = []
         for filename in local.keys():
             if filename in server.keys():
-                if not local[filename][0] ==  server[filename][0]:
+                if not local[filename][0] == server[filename][0]:
                     if local[filename][1] > server[filename][1]:
-                        serverUpdates.add(filename) # add file to be updated on server
+                        serverUpdates.append(filename)  # add file to be updated on server
                     elif local[filename][1] < server[filename][1]:
-                        localUpdates.add(filename) # add file to be updated on local machine
-            #TODO make variable for length of time of polling
-            elif filename[1] > (time.gmtime() - 300000): #file was modified since last update; should be saved to server
-                serverUpdates.add(filename)
+                        localUpdates.append(filename)  # add file to be updated on local machine
+
+            # file was modified since last update; should be saved to server
+            elif filename[1] > (time.gmtime() - (self.interval * 60)):
+                serverUpdates.append(filename)
         for filename in server.keys():
-            if not filename in local.keys() & filename[1] > (time.gmtime() - 300000):  #file was created elsewhere since last update; should be saved to local
-                localUpdates.add(filename)  # add file to be updated on local machine
+            # file was created elsewhere since last update; should be saved to local
+            if not filename in local.keys() & filename[1] > (time.gmtime() - (self.interval * 60)):
+                localUpdates.append(filename)  # add file to be updated on local machine
         return [localUpdates, serverUpdates]
 
     #Unifies above methods
-    def check_updates(self, path):
-        localManifest = self.get_local_files()
+    def check_updates(self):
+        localManifest = self.get_local_files(self.path)
         serverManifest = self.get_server_files()
         return self.compareManifests(localManifest, serverManifest)
 
     #runner for file updates
-    def run_file_updates(self ):
+    def run_file_updates(self):
 
         #delete excludeFiles from server
 
-        if self.check_directory(self.path):
-            updateFiles = self.check_updates(self.path)
+        if self.check_directory():
+            updateFiles = self.check_updates()
             localUpdates = updateFiles[0]
             serverUpdates = updateFiles[1]
-        else: #Directory doesn't exist, so no local files are on machine
-            serverUpdates = {} # No local files on machine means no updates need to be made to server
-            localUpdates = self.get_server_files() # Local machine needs all files from server
+        else:  # Directory doesn't exist, so no local files are on machine
+            serverUpdates = {}  # No local files on machine means no updates need to be made to server
+            localUpdates = self.get_server_files()  # Local machine needs all files from server
         if localUpdates:
             # download all files in localUpdates
             #TODO add this in later
@@ -124,8 +130,8 @@ class FileChecker:
     def poll_file_updates(self):
         while True:
             self.run_file_updates()
-            oldFiles = self.get_local_files()
-            time.sleep(self.interval*60) #sleeps for interval minutes
+            time.sleep(self.interval * 60)  # sleeps for interval minutes
+
 
 #Set your user path here (Could come from config file later)
 #Creates a process to run in background and polls for file updates
@@ -134,9 +140,8 @@ def main():
     pathname = userhome + '/Onedir'
     interval = 5
     checkMe = FileChecker(pathname, interval)
-    t = threading.Thread(target=FileChecker.poll_file_updates, args=())
+    t = threading.Thread(target=checkMe.poll_file_updates, args=())
     t.start()
 
-
-if __name__== '__main__':
+if __name__ == '__main__':
     main()
--- prototypes/test_file_updates.py
+++ prototypes/test_file_updates.py
@@ -4,6 +4,7 @@ __author__ = 'robert'
 import unittest
 from file_updates import FileChecker
 
+
 class TestFileUpdates (unittest.TestCase):
     def testInitNoArgs(self):
         fileChecker = FileChecker()
