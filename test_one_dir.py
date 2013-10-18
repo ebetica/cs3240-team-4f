9677a37a98b2e9882be735ea849cc2b1a3d63998
MisterAbc <zl4ry@virginia.edu>
2013-10-18 15:20:52 -0400

    More tests

--- test_one_dir.py
+++ test_one_dir.py
@@ -19,15 +19,38 @@ class TestOneDir(unittest.TestCase):
         self.assertEqual(self.user_in_database("not_a_user"), FALSE)
 
 
+    def test_register_login(self):
+        name = "test_user"
+        password = "password"
+        self.assertEqual(self.register_user(name, password), TRUE)
+        self.assertEqual(self.login(name, password), TRUE)
+        self.assertEqual(self.login(name, "wrongpassword"), FALSE)
+        self.assertEqual(self.login("not-in-database", password), FALSE)
+
+
     def tearDown(self):
         os.close(self.db_fd)
         os.unlink(server.app.config['DATABASE'])
 
 
     def user_in_database(self, username):
-        ret = self.app.post('/user_in_database', data=dict(
+        ret = self.app.get('/user_in_database', data=dict(
                     username=username), follow_redirects=True).data
         print(self.app.get.__doc__)
+        return ret
+
+
+    def register_user(self, username, password):
+        ret = self.app.post('/register', data=dict(
+                    username=username, password=password)
+                    , follow_redirects=True).data
+        return ret
+
+
+    def login(self, username, password):
+        ret = self.app.post('/register', data=dict(
+                    username=username, password=password)
+                    , follow_redirects=True).data
         return ret
         
 
