3891be355f135065dfed8e5197d9c2f3b544c039
robert <roe2pj@virginia.edu>
2013-10-20 11:40:57 -0400

    Added server address, formatted a bit because I'm OCD

--- prototypes/updown.py
+++ prototypes/updown.py
@@ -1,22 +1,26 @@
 __author__ = 'rbq2rd'
-import os
-from flask import Flask, request
+
+import constants
 import requests
 
-def upload_file(self, url, filename):
+
+def upload_file(url, filename):
     files = {'file': open(filename,'rb')}
     r = requests.post(url,files=files)
-def download_file(self,url,filename):
+
+
+def download_file(url,filename):
     r = requests.get(url)
     with open(filename, 'wb') as code:
         code.write(r.content)
 
+
 def main():
-    url = '' # wherever our server is
-    filename = '' #some file
-    #uploads filename to url
+    url = constants.SERVER_ADDRESS # wherever our server is
+    filename = '' # some file
+    # uploads filename to url
     upload_file(url, filename)
-    #downloads the filename uploaded to url and names it ;'banana.txt'
+    # downloads the filename uploaded to url and names it ;'banana.txt'
     download_file(url, 'banana.txt')
 
 
